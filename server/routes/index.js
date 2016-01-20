bff3d559d6f5c5fe0bc5889a9585036780104845
Derek Roche <ddrochedsp@gmail.com>
2016-01-20 13:07:51 -0600

    Fixed asset serving bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,8 @@ var path = require('path');
 
 router.get('/*', function(req, res) {
   var file = req.params[0] || 'views/index.html';
-  res.sendFile(path.join(__dirname, '../../public/', file));
+  res.sendFile(path.join(__dirname, '../../public', file));
 });
+
 
 module.exports = router;
