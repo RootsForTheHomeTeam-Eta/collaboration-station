c2d8aeabdb8154203927b5fb29f6a15c033e7063
Derek Roche <ddrochedsp@gmail.com>
2016-01-20 11:47:50 -0600

    Auth configuration in process

---
+++ server/routes/register.js
@@ -0,0 +1,17 @@
+// login route
+var express = require('express');
+var router = express.Router();
+var passport = require('passport');
+
+// use jwt-login passport strategy to authenticate login
+router.post('/', passport.authenticate('jwt-register',
+  // do not use sessions (we are using tokens)
+  {session: false}
+  ),
+  // upon authentication, send profile
+  function(req, res) {
+    res.send(req.user.profile);
+  }
+);
+
+module.exports = router;
