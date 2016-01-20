1bd7f0ea2a8ecf1e552bcb4e7e233466312a800f
Amanda Bausch <manadab@me.com>
2016-01-20 15:05:57 -0600

    partial routes

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,12 @@
 var app = angular.module('rootsApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: '/views/partials/index.html'
+    }).when('/home', {
+        templateUrl: '/views/partials/home.html'
+    }).when('/login', {
+        templateUrl: '/views/partials/login.html',
+        controller: 'loginCtrl'
+    });
+}]);
