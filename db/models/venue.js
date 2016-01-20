54a99064d894127f8ec789a76462f2c0a35f7200
Derek Roche <ddrochedsp@gmail.com>
2016-01-20 14:21:39 -0600

    Updated Venue Schema

--- db/models/venue.js
+++ db/models/venue.js
@@ -4,11 +4,24 @@ var Schema = mongoose.Schema;
 var VenueSchema = new Schema({
     venueName: String,
     location: String,
-    date: [{type:Date: [{
-                type:schema.Types.ObjectId, ref:'Users'[{first: Boolean}, {second:Boolean}, {cannot:Boolean}]},
-                arrivalTime:Date,
-                gameTime:Date}];
-
+    //eventDate: [{type:Date: [{
+    //            type:schema.Types.ObjectId, ref:'Users'[{first: Boolean}, {second:Boolean}, {cannot:Boolean}]},
+    //            arrivalTime:Date,
+    //            gameTime:Date}],
+    events: [{
+      event: {
+        eventDate: Date,
+        preferences: [{
+          orgName: [{
+            first: {type: Boolean, default: false},
+            second: {type: Boolean, default: false},
+            cannot: {type: Boolean, default: false}
+          }]
+        }]
+      },
+      arrivalTime: Date,
+      gameTime: Date
+    }]
 });
 
 module.exports = mongoose.model("Venue", VenueSchema);
