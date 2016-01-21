67f695387d263f21775061c5f395279c0b0cd8dd
Amanda Bausch <manadab@me.com>
2016-01-21 14:13:49 -0600

    admin page set up a styling in progress

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -24,3 +24,23 @@ rootsAppControllers.controller('mainCtrl', function ($scope) {
   $scope.hello = 'hello!';
   console.log($scope.hello);
 });
+
+
+//controller for tabs
+
+rootsAppControllers.controller('tabController', function ($scope){
+  $scope.currentTab = null;
+  //setting these functions as part of the scope makes the function available
+  //to process in-line via ng-click
+  //onTabClick sets the current tab to the id of the tab that was clicked
+  $scope.onTabClick = function(id) {
+    $scope.currentTab = id;
+  };
+  //isActive sets the currentTab to active so that
+  //the appropriate class is assigned via ng-class
+  //ng-show reads if the is-active function is true and
+  //only shows when it is.
+  $scope.isActive = function(id) {
+    return $scope.currentTab === id;
+  };
+});
