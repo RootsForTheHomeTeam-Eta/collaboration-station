06cd3a60432e77a023a29dcd5c66143782cf924c
Amanda Bausch <manadab@me.com>
2016-01-25 21:55:40 -0600

    event post route clientSide started, schedule save Post route started, venue get route started

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -47,10 +47,8 @@ rootsAppControllers.controller('tabController', function ($scope){
 
 //controllers for admin forms
 
-
-//new event submit controller REALLY Unsure on how this will
-//work with our data model Would LOVE a walk through
-//just wanted to get things started on my end
+//new event submit controller will this
+//work with our data model
 rootsAppControllers.controller("formEventCtrl", ['$scope', '$http', function($scope, $http) {
     $scope.event = {};
   //form data tied to model where possible
@@ -72,30 +70,18 @@ rootsAppControllers.controller("formEventCtrl", ['$scope', '$http', function($sc
 
       });
 
-      //this may do nothing
-      $http({
-        url: '/event',
-        //we need a post route for /event
-        method: 'get'
-      }).then(function(res){
-        $scope.event = res.data;
-      });
     };
-  //this will of course go...
-  $scope.hello = 'hello!';
-  console.log($scope.hello);
   //should have a popupS modal confirmation
 
 }]);
 
 
-//new user creation controller REALLY Unsure on how this will
-//work with our data model Would LOVE a walk through
-//just wanted to get things started on my end
+//new user creation controller how will this
+//work with our data model
 rootsAppControllers.controller("userCtrl", ['$scope', '$http', function($scope, $http) {
   $scope.user = {};
   $scope.submit = function () {
-    //form data tied to model where possible
+    //form data tied to model
     var user = {
       orgName: $scope.orgName,
       firstName: $scope.firstName,
@@ -111,40 +97,22 @@ rootsAppControllers.controller("userCtrl", ['$scope', '$http', function($scope, 
 
     });
 
-    //this may do nothing
-    $http({
-      url: '/user',
-      //we need a post route for /user
-      method: 'get'
-    }).then(function(res){
-      $scope.user = res.data;
-    });
   };
-  //this will of course go...
-  $scope.hello = 'hello!';
-  console.log($scope.hello);
   //should have a popupS modal confirmation
 
 }]);
 
 
-
-
-//Not sure on these
 rootsAppControllers.controller('noticeAlertCtrl', function ($scope) {
   //alert should appear when activity is made on
-  //form submission or email
-  //when pressed should expose what type of activity was made
-  //or contents of message in a popupS modal
+  //form submission
   $scope.hello = 'hello!';
   console.log($scope.hello);
 });
 
 rootsAppControllers.controller('noticeSendCtrl', function ($scope) {
-  //Send notices with touch of button based on who is selected
-  //and what type of message
+  //Send notices with button based on who is selected and what type of message
   //should have a popupS modal confirmation
-
   $scope.hello = 'hello!';
   console.log($scope.hello);
 });
@@ -157,11 +125,38 @@ rootsAppControllers.controller('messageCtrl', function ($scope) {
 });
 
 
-//does each click need it's own controller?
 rootsAppControllers.controller('scheduleController', function ($scope) {
+  $http({
+    method: 'GET',
+    url: '/venue'
+    //all info from venues is available via this request
+  }).then(function(res){
+    $scope.venueName = res.data.venueName;
+    $scope.eventDate = res.data.events.eventDate;
+    $scope.arrivalTime = res.data.arrivalTime;
+    $scope.eventTime= res.data.gameTime;
+  });
+
   //will save current schedule with ng-click saveSchedule()
   $scope.saveSchedule= function(){
-    alert("I clicked save schedule")
+    var schedule = {
+      venue: $scope.venueName,
+      events: [{
+        event: {
+          eventDate: $scope.eventDate,
+          arrivalTime: $scope.arrivalTime,
+          eventTime: $scope.eventTime,
+          orgName: $scope.orgName
+        }
+      }]
+    };
+    $http({
+      method: 'POST',
+      url: '/schedule',
+      data: schedule
+    }).then(function () {
+
+    });
   };
   //will view previous schedule with ng-click viewPreviousSchedule()
   $scope.viewPreviousSchedule= function(){
