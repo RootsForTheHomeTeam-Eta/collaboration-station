6c87d444934c6021b88c6e2e30cfc89d6b1d70bd
Manuel Alvarez <zuma3434@gmail.com>
2016-01-28 12:20:33 -0600

    added sync logic to GetEventsFactory

--- public/assets/js/Controllers/ScheduleController.js
+++ public/assets/js/Controllers/ScheduleController.js
@@ -1,12 +1,13 @@
 //Controller to populate schedule creation bars on admin page
 rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory', '$log', function($scope, $http, VenueEventsFactory, $log) {
-    VenueEventsFactory.getVenues()
-      .success(function(venues){
-          //$scope.$apply(function() {
-              $scope.venues = venues;
-          //})
-      })
-      .error(function(data, status, headers, config) {
-          $log.warn(data, status, headers(), config);
-      });
+
+
+    $scope.venues = VenueEventsFactory.venues;
+
+    VenueEventsFactory.getVenues();
+
+    console.log(VenueEventsFactory.venues);
+
+    console.log($scope.venues);
+
 }]);
