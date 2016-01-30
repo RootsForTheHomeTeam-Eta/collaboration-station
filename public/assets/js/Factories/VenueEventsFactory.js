fdda873edb26361b724fbf2108b7c7f4c72c7439
Manuel Alvarez <zuma3434@gmail.com>
2016-01-30 02:01:40 -0600

    Ng-repeat radio buttons console logging on submit, changed way factory is injected in admin schedule & user form

--- public/assets/js/Factories/VenueEventsFactory.js
+++ public/assets/js/Factories/VenueEventsFactory.js
@@ -2,23 +2,47 @@
 // creates a service that shares the venue object between controllers
 rootsApp.factory('VenueEventsFactory', function($http) {
 
-    var venues = [];
-
-    return{
-        getVenues : function() {
-            return $http({
+        var getVenues = function() {
+            $http({
                 url: '/api/event/getEvents',
                 method: 'GET'
             }).success(function(result){
-                    venues = result;
-                    console.log(venues);
+                    venues.data = result;
+                    console.log(venues.data);
 
                 })
                 .error(function(data, status, headers, config) {
                     $log.warn(data, status, headers(), config);
                 });
-        },
-        venues: venues
+        };
 
-    };
+    var venues = {};
+
+    return {
+        getVenues : getVenues,
+        venues : venues
+    }
 });
+
+
+//rootsApp.factory('VenueEventsFactory', function($http) {
+//
+//    var venues = {};
+//
+//    return{
+//        getVenues : function() {
+//            return  $http({
+//                url: '/api/event/getEvents',
+//                method: 'GET'
+//            }).success(function(result){
+//                    venues.data = result;
+//                    console.log(venues.data);
+//
+//                })
+//                .error(function(data, status, headers, config) {
+//                    $log.warn(data, status, headers(), config);
+//                });
+//        },
+//        venues: venues
+//    };
+//});
