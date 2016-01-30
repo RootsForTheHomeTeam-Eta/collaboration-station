67dd95f14c72957820636e28985f372680262a53
Amanda Bausch <manadab@me.com>
2016-01-29 23:29:56 -0600

    schedule dating saving venue name with init but doesn't work with repeat

--- public/assets/js/Controllers/SaveController.js
+++ public/assets/js/Controllers/SaveController.js
@@ -1,33 +1,33 @@
 //controller to save admin created schedule in the database
 rootsApp.controller('SaveController', ['$scope', '$http', function($scope, $http) {
-    //will save current schedule with ng-click saveSchedule()
-    $scope.schedule = {};
-
-    $scope.saveNewSchedule = function () {
-
-        var schedule = {
-            finalVenue: $scope.finalVenue,
-            events: [{
-                event: {
-                    finalEventDate: $scope.finalEventDate,
-                    finalArrivalTime: $scope.finalArrivalTime,
-                    finalEventTime: $scope.finalEventTime,
-                    finalOrgName: $scope.finalOrgName
-                }
-            }]
-        };
-        console.log('logging '+ $scope.schedule);
-        console.log($scope.finalVenue);
-
-        $http({
-            method: 'POST',
-            url: '/saveSchedule',
-            data: schedule
-        }).then(function (schedule) {
-            console.log(schedule);
-        });
-        //popupS.alert({
-        //    content: 'Schedule Saved'
-        //});
-    };
+    ////will save current schedule with ng-click saveSchedule()
+    //$scope.schedule = {};
+    //
+    //$scope.saveNewSchedule = function () {
+    //    console.log($scope.schedule.finalVenue);
+    //    var schedule = {
+    //        finalVenue: $scope.schedule.finalVenue,
+    //        events: [{
+    //            event: {
+    //                finalEventDate: $scope.finalEventDate,
+    //                finalArrivalTime: $scope.finalArrivalTime,
+    //                finalEventTime: $scope.finalEventTime,
+    //                finalOrgName: $scope.finalOrgName
+    //            }
+    //        }]
+    //    };
+    //    console.log('logging '+ $scope.schedule);
+    //    console.log($scope.finalVenue);
+    //
+    //    $http({
+    //        method: 'POST',
+    //        url: '/saveSchedule',
+    //        data: schedule
+    //    }).then(function (schedule) {
+    //        console.log(schedule);
+    //    });
+    //    //popupS.alert({
+    //    //    content: 'Schedule Saved'
+    //    //});
+    //};
 }]);
