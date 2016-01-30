652778067a129d8940be8cad6c198c94fab908cb
Amanda Bausch <manadab@me.com>
2016-01-30 13:40:49 -0600

    data submit begining functionality on admin for save schedule- needs preferences and groups brough in

--- public/assets/js/Controllers/ScheduleController.js
+++ public/assets/js/Controllers/ScheduleController.js
@@ -3,7 +3,10 @@
 rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory', '$log', function($scope, $http, VenueEventsFactory, $log) {
 
     $scope.venues = VenueEventsFactory.venues;
+
     VenueEventsFactory.getVenues();
+
+
 
     //VenueEventsFactory.getVenues().then(function(result) {
     //    $scope.venues = result;
@@ -12,31 +15,41 @@ rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory'
     //will save current schedule with ng-click saveSchedule()
     $scope.schedule = {};
 
-    $scope.saveNewSchedule = function (finalVenueNameValue, finalEventDateValue) {
-
-        console.log(finalVenueNameValue);
-        console.log(finalEventDateValue);
-        var schedule = {
-            finalVenue: finalVenueNameValue,
-            events: [{
-                event: {
-                    finalEventDate: finalEventDateValue,
-                    finalArrivalTime: $scope.finalArrivalTime,
-                    finalEventTime: $scope.finalEventTime,
-                    finalOrgName: $scope.finalOrgName
-                }
-            }]
-        };
-
-        $http({
-            method: 'POST',
-            url: '/saveSchedule',
-            data: schedule
-        }).then(function (schedule) {
-
-        });
-        //popupS.alert({
-        //    content: 'Schedule Saved'
+    $scope.submit = function () {
+        console.log($scope.schedule);
+        // $http({
+        //     url: '/api/user/submit',
+        //     method: 'post'
+        // }).then(function (res) {
+        // $log.info(res.status);
         //});
     };
+
+    //$scope.saveNewSchedule = function (finalVenueNameValue, finalEventDateValue) {
+    //
+    //    console.log(finalVenueNameValue);
+    //    console.log(finalEventDateValue);
+    //    var schedule = {
+    //        finalVenue: finalVenueNameValue,
+    //        events: [{
+    //            event: {
+    //                finalEventDate: finalEventDateValue,
+    //                finalArrivalTime: $scope.finalArrivalTime,
+    //                finalEventTime: $scope.finalEventTime,
+    //                finalOrgName: $scope.finalOrgName
+    //            }
+    //        }]
+    //    };
+    //
+    //    $http({
+    //        method: 'POST',
+    //        url: '/saveSchedule',
+    //        data: schedule
+    //    }).then(function (schedule) {
+    //
+    //    });
+    //    //popupS.alert({
+    //    //    content: 'Schedule Saved'
+    //    //});
+    //};
 }]);
