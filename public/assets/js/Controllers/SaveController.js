dbafd68ac6655e478c57f55e72430e98592d2025
Amanda Bausch <manadab@me.com>
2016-01-29 22:51:38 -0600

    Schedule not saving - Schedule bars no longer appear upon submit

--- public/assets/js/Controllers/SaveController.js
+++ public/assets/js/Controllers/SaveController.js
@@ -1,27 +1,29 @@
 //controller to save admin created schedule in the database
 rootsApp.controller('SaveController', ['$scope', '$http', function($scope, $http) {
     //will save current schedule with ng-click saveSchedule()
+    $scope.schedule = {};
 
-    var schedule = {
-        finalVenue: $scope.finalVenueName,
-        events: [{
-            event: {
-                finalEventDate: $scope.finalEventDate,
-                finalArrivalTime: $scope.finalArrivalTime,
-                finalEventTime: $scope.finalEventTime,
-                finalOrgName: $scope.finalOrgName
-            }
-        }]
-    };
+    $scope.saveNewSchedule = function () {
 
-    $scope.saveSchedule = function () {
+        var schedule = {
+            finalVenue: $scope.finalVenue,
+            events: [{
+                event: {
+                    finalEventDate: $scope.finalEventDate,
+                    finalArrivalTime: $scope.finalArrivalTime,
+                    finalEventTime: $scope.finalEventTime,
+                    finalOrgName: $scope.finalOrgName
+                }
+            }]
+        };
+        console.log('logging '+ $scope.schedule);
+        console.log($scope.finalVenue);
 
         $http({
             method: 'POST',
             url: '/saveSchedule',
             data: schedule
         }).then(function (schedule) {
-            console.log('after post');
             console.log(schedule);
         });
         //popupS.alert({
--- server/routes/saveSchedule.js
+++ server/routes/saveSchedule.js
@@ -16,7 +16,7 @@ router.post('/', function(req, res, next) {
         finalOrgName: req.body.finalOrgName
     };
     console.log('hit the post');
-    console.log(schedule.finalOrgName);
+    //console.log(schedule.finalOrgName);
     console.log(schedule.finalVenue);
 
     Schedule.findOneAndUpdate(
