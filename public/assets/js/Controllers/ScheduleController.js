b762f88960848d925acdd4ace54d33c9d3a56a7e
Amanda Bausch <manadab@me.com>
2016-01-31 10:14:56 -0600

    I have data but am not sure where to go from here.

--- public/assets/js/Controllers/ScheduleController.js
+++ public/assets/js/Controllers/ScheduleController.js
@@ -1,4 +1,3 @@
-
 //Controller to populate schedule creation bars on admin page
 rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory', '$log', function($scope, $http, VenueEventsFactory, $log) {
 
@@ -6,32 +5,49 @@ rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory'
 
     VenueEventsFactory.getVenues();
 
+    //VenueEventsFactory.getVenues().then(function(result) {
+    //    $scope.venues = result;
+    //});
+
     //will save current schedule with ng-click saveSchedule()
     $scope.formData = {};
 
-    $scope.submitAndSave = function () {
-        console.log($scope.formData);
 
-    //    var schedule ={
-    //        finalVenue: ,
-    //            events: [{
-    //                event: {
-    //                    finalEventDate: ,
-    //                    finalArrivalTime: $scope.finalArrivalTime,
-    //                    finalEventTime: $scope.finalEventTime,
-    //                    finalOrgName: $scope.finalOrgName
-    //                    }
-    //                }]
-    //
-    //    };
-    //     $http({
-    //         url: '/saveSchedule',
-    //         method: 'post'
-    //     }).then(function (res) {
-    //     $log.info(res.status);
-    //    });
+    //var schedule = {
+    //    finalVenue: $scope.formData[i].venueName,
+    //    events: [{
+    //        event: {
+    //            finalEventDate: $scope.formData[i].events[n].eventDate,
+    //            finalArrivalTime: $scope.formData[i].events[n].arrivalTime,
+    //            finalEventTime: $scope.formData[i].events[n].gameTime,
+    //            finalOrgName: $scope.formData[i].events[n].orgName
+    //        }
+    //    }]
     //};
 
+    $scope.submitAndSave = function () {
+
+        var $schedules = [];
+
+        for (i in $scope.formData) {
+
+            var $schedule = {
+                venueName: $scope.formData[i].venueName,
+                events: $scope.formData[i].events
+            };
+            $schedules.push($schedule);
+        }
+        console.log($schedules);
+        console.log("save button clicked");
+
+    //    $http({
+    //        url: '/api/user/submit',
+    //        method: 'post',
+    //        data: $schedules;
+    //}).then(function (res) {
+    //        $log.info(res.status);
+    //    });
 
     };
+
 }]);
