a23d2944d0c3a90cab2012340263bf69945fe008
Manuel Alvarez <zuma3434@gmail.com>
2016-02-01 13:11:20 -0600

    testing

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,12 +5,48 @@ var Venue = require('../../db/models/venue');
 var User = require('../../db/models/venue');
 
 router.post('/submit', function(req, res, next) {
-  // submit the data
-  // iterate through data
-  // for each venue
-  // // find the venue, modify the organization prefs
-  console.log(req.body);
-  res.sendStatus(200);
+
+    console.log("IN SUB");
+    console.log("req.body show :", req.body[0]);
+
+    for (i in req.body) {
+        _id : req.body[i].eventId
+        preferences.first :req.body[i].preferences.first
+
+    }
+
+    //var _id = req.body.0,
+   //preferences: req.body.1.preferences.first
+
+  //Venue.findOneAndUpdate(
+  //    // query
+  //    {_id: userSubmit._id},
+  //    // doc to update
+  //    {$push: {
+  //      events: {
+  //        event: {
+  //            preferences: {
+  //                first: event.preferences
+  //            }
+  //        }}
+  //    }},
+  //    // options to update with
+  //    {
+  //      new: true,
+  //      upsert: true
+  //    },
+  //    // callback
+  //    function(err, doc) {
+  //      if (err) {
+  //        console.log(err);
+  //        next(err);
+  //      }
+  //      console.log(doc);
+  //      res.json(doc);
+  //    }
+  //);
+    res.sendStatus(200);
+
 });
 
 module.exports = router;
