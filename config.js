f244250515ca266be15c2fcc70f5a3086737e9d0
Derek Roche <ddrochedsp@gmail.com>
2016-02-02 09:00:00 -0600

    Working Data

--- config.js
+++ config.js
@@ -4,10 +4,18 @@ module.exports = {
   'MONGOURI': 'mongodb://localhost:27017/roots_app'
 };
 
-// script to create admin user:
+//// script to create admin user:
 //db.users.insert({"orgName": "Roots for the Home Team",
-//                 "firstName": "Susan",
-//                 "lastName": "Moores",
+//                 "firstName": "Admin",
+//                 "lastName": "User",
 //                 "username": "admin@testing.com",
 //                 "password": "testing",
 //                 "isAdmin": "true"});
+//
+//// script to create test user
+//db.users.insert({"orgName": "Prime Digital Academy",
+//                 "firstName": "Test",
+//                 "lastName": "User",
+//                 "username": "user@testing.com",
+//                 "password": "testing",
+//                 "isAdmin": "false"});
