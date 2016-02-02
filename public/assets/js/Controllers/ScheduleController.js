580debe6b8d92be354c345ad06582d9aa2a86a66
Amanda Bausch <manadab@me.com>
2016-02-02 16:59:49 -0600

    working on loop for preferences

--- public/assets/js/Controllers/ScheduleController.js
+++ public/assets/js/Controllers/ScheduleController.js
@@ -7,7 +7,24 @@ rootsApp.controller('ScheduleController',['$scope','$http', 'VenueEventsFactory'
 
     //this object is filled by the scope setting we did in the html so that we could deal with the
     //loops easier
-    $scope.formData = {};
+
+    var arrayOrgs = ["Appetite for Change","Dream of Wild Health","Urban Roots","Youth Farm Frogtown","Youth Farm Hawthorn","Youth Farm Lyndale","Youth Farm Powderhorn","Youth Farm W.Side"];
+    $scope.$arrayOrgs = arrayOrgs;
+
+    //we need an array to repeat on to create the empty divs for our view structure
+
+    //this won't work because we have a set amount of organizations and they won't populate in
+    //a guranteed order.
+    // var orgNameUnit;
+    //var organizationsArray = [];
+    //
+    //
+    //for (var i = 0; i < venues.events.event.organization.length ; i++) {
+    //    orgNameUnit = venues.events.event.organization.orgName;
+    //    organizationsArray.push(orgNameUnit);
+    //    }
+    //
+    //$scope.organizationsArray = organizationsArray;
 
 
     $scope.submitAndSave = function () {
