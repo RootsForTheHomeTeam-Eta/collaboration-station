f109edb6a9857a88c55f56995c46483ac871fd06
Manuel Alvarez <zuma3434@gmail.com>
2016-02-03 10:29:59 -0600

    added notifiction route and displays alerts on user form submit

--- public/assets/js/Controllers/NoticeAlertController.js
+++ public/assets/js/Controllers/NoticeAlertController.js
@@ -1,9 +1,11 @@
 //controller to populate alerts based on user form submission
-rootsApp.controller('NoticeAlertController',['$scope','messages', function ($scope, messages) {
+rootsApp.controller('NoticeAlertController',['$scope','User2AdminFactory', function ($scope, User2AdminFactory) {
     //alert should appear when activity is made on
     //form submission
-    var self = this;
 
-    self.messages = messages.list;
+    $scope.notifications = User2AdminFactory.notifications;
+    User2AdminFactory.getNotifications();
+    console.log($scope.notifications);
+
 
 }]);
