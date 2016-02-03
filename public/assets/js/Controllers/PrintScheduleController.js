dd35ec000a3ebdc4886280b6ad41b8a05d2b8239
Keisha Josephs <kmjosephs@gmail.com>
2016-02-03 16:29:15 -0600

    new controller to print schedule

---
+++ public/assets/js/Controllers/PrintScheduleController.js
@@ -0,0 +1,16 @@
+// $window is an Angular service and needs to be put in the angular config
+
+rootsApp.controller('PrintScheduleController', ['$scope', function($scope){
+    console.log('Printing from PrintScheduleController.js');
+    $scope.printSchedule = function(){
+
+        // need an id on the schedule html, 'printArea' was used on Stack Overflow
+        var schedule = document.getElementById('printArea').innerHTML;
+        // the first two arguments to $window.open are a URL and a name.
+        //these were left blank but I imagine we could put it in later
+        var scheduleWindow = $window.open('', '', 'width=800', 'height=600');
+        scheduleWindow.document.write(schedule);
+        scheduleWindow.print();
+    }
+
+}]);
