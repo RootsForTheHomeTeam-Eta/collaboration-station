7d7bfd287a317cdef4f0ac62a3db1c67bc0a88cb
Derek Roche <ddrochedsp@gmail.com>
2016-02-05 09:59:14 -0600

    Updated sendNotices and sendQuickMail server responses

--- server/routes/sendNotices.js
+++ server/routes/sendNotices.js
@@ -1,37 +1,43 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var config = require('../../config');
 
 
 router.post('/', function (req, res) {
     var mailOpts, smtpTrans;
+    var recipient = req.body.sendToemail;
+    var message = req.body.sendTomessage;
+    console.log(req.body);
 
     smtpTrans = nodemailer.createTransport('Gmail', {
         service: 'Gmail',
         auth: {
-            user: "", //will be sue in hidden variable
-            pass: ""
+            user: config.GMAIL.USER, //will be sue in hidden variable
+            pass: config.GMAIL.PASS
         }
     });
     //Mail options
     mailOpts = {
         from: "",//will be sue in hidden variable
-        to: req.body.sendToemail,
+        to: recipient,
         subject: 'A message from Roots for the Home Team',
-        text: req.body.sendTomessage + '\n from ' + "Sue" + '\n at ' + "Roots for the Home Team"
+        text: message + '\n from ' + "Sue" + '\n at ' + "Roots for the Home Team"
     };
-    smtpTrans.sendMail(mailOpts, function (error, res) {
+    smtpTrans.sendMail(mailOpts, function (error, response) {
         //Email not sent
         if (error) {
-            console.log(error + 'you goofed');
+            res.sendStatus(400);
+            console.log(error + ' you goofed');
         }
         //Email sent
         else {
-            console.log('whoosh');
+            res.sendStatus(200);
+            console.log('whoosh!');
         }
     });
     //not sure what this should be...confirmation alert?
-    smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
+    smtpTrans.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
 });
 
 module.exports = router;
