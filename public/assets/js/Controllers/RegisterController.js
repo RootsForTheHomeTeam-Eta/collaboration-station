59ff639a5f1f3b274022a8ecd77afb609c52ada2
Amanda Bausch <manadab@me.com>
2016-02-07 17:20:43 -0600

    reset/require work. can have one or the other right now

--- public/assets/js/Controllers/RegisterController.js
+++ public/assets/js/Controllers/RegisterController.js
@@ -1,6 +1,15 @@
 //controller for creating new users on the database
 rootsApp.controller("RegisterController", ['$scope', '$http', function($scope, $http) {
     $scope.user = {};
+    $scope.reset = function() {
+        $scope.orgName = '';
+        $scope.firstName = '';
+        $scope.lastName = '';
+        $scope.userName = '';
+        $scope.password = '';
+    };
+    //reset not working in this model
+    //reset can wokr but them require doesn't
     $scope.register = function () {
         //form data tied to model
         var user = {
@@ -23,5 +32,7 @@ rootsApp.controller("RegisterController", ['$scope', '$http', function($scope, $
         });
     };
     //should have a popupS modal confirmation
+
+
 
 }]);
--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -361,6 +361,15 @@ rootsApp.controller('PrevController', ['$scope', '$http', function($scope, $http
 //controller for creating new users on the database
 rootsApp.controller("RegisterController", ['$scope', '$http', function($scope, $http) {
     $scope.user = {};
+    $scope.reset = function() {
+        $scope.orgName = '';
+        $scope.firstName = '';
+        $scope.lastName = '';
+        $scope.userName = '';
+        $scope.password = '';
+    };
+    //reset not working in this model
+    //reset can wokr but them require doesn't
     $scope.register = function () {
         //form data tied to model
         var user = {
@@ -383,6 +392,8 @@ rootsApp.controller("RegisterController", ['$scope', '$http', function($scope, $
         });
     };
     //should have a popupS modal confirmation
+
+
 
 }]);
 //Controller to populate schedule creation bars on admin page
