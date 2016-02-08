258491bcd06f9697f5460bad7490a5e37e8f015d
Amanda Bausch <manadab@me.com>
2016-02-07 20:19:45 -0600

    styling print view

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -140,7 +140,7 @@ rootsApp.controller('MainHelpController', ['$rootScope', '$scope', '$http', '$lo
 
             popupS.modal({
                 content:'<div class="helpBody">' +
-                '<h2 class="helpHeader">Need a reminder?</h2>'+
+                '<h2 class="helpHeader">Need Some Help?</h2>'+
                 '<p>The "Alerts" tab will show you what group has submitted their schedule form. Clear the alert by clicking the alert button.</p> ' +
                 '<p>The "Quick Send" tab allows you to select Garden Groups to send specific quick alerts to. Select the recipient or recipients, the type of alert and submit.</p> ' +
                 '<p>Add new events to the schedule by filling in the "Add Event" form. These events will be used to create the new form for the garden' +
@@ -149,8 +149,9 @@ rootsApp.controller('MainHelpController', ['$rootScope', '$scope', '$http', '$lo
                 'that you can share with them. </p> ' +
                 '<p>The lower section of the page contains a schedule view. Click the Orange bar with the venue for the event you would like to schedule. ' +
                 'See users choices and click on the appropriate button for who you would like to schedule during the selected event. Only one group may be ' +
-                'selected for each event time. Once you have chosen the groups for the events at a venue save the schedule. You can noe download, print and share' +
-                ' the event schedule.</p> ' +
+                'selected for each event time. Once you have chosen the groups for the events at a venue save the schedule. Once saved, ' +
+                'press the view schedule button to see the created schedule. You can print via the print button or your browsers print option, you can also share the ' +
+                'link to share the event schedule.</p> ' +
                 '</div>'
             });
 
