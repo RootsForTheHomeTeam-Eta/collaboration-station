3b37496b9d61e225ddb7e53e36bb6ada116dee92
Derek Roche <ddrochedsp@gmail.com>
2016-02-16 14:07:58 -0600

    routing change

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,18 +3,18 @@ var rootsApp = angular.module('rootsApp', ['ngRoute']);
 // app router
 rootsApp.config(['$routeProvider', '$httpProvider', function($routeProvider){
     $routeProvider.when('/admin', {
-        templateUrl: 'views/partials/admin.html',
+        templateUrl: '/views/partials/admin.html',
         controller: 'AdminViewController'
     }).when('/login', {
-        templateUrl: 'views/partials/login.html',
+        templateUrl: '/views/partials/login.html',
         controller: 'LoginController'
     }).when('/logout', {
         controller: 'LogoutController'
     }).when('/user', {
-        templateUrl: 'views/partials/userView.html',
+        templateUrl: '/views/partials/userView.html',
         controller: 'UserScheduleFormSubmitController'
     }).when('/tableview', {
-        templateUrl: 'views/partials/tableview.html',
+        templateUrl: '/views/partials/tableview.html',
         controller: 'FinalScheduleViewController'
     }).otherwise({
         redirectTo: 'login'
