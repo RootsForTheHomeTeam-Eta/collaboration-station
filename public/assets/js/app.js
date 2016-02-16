4dfd9594799ef8dbad6a47293fc343bf9ce53302
Derek Roche <ddrochedsp@gmail.com>
2016-02-16 14:11:46 -0600

    routing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,21 +3,21 @@ var rootsApp = angular.module('rootsApp', ['ngRoute']);
 // app router
 rootsApp.config(['$routeProvider', '$httpProvider', function($routeProvider){
     $routeProvider.when('/admin', {
-        templateUrl: '/views/partials/admin.html',
+        templateUrl: 'public/views/partials/admin.html',
         controller: 'AdminViewController'
     }).when('/login', {
-        templateUrl: '/views/partials/login.html',
+        templateUrl: 'public/views/partials/login.html',
         controller: 'LoginController'
     }).when('/logout', {
         controller: 'LogoutController'
     }).when('/user', {
-        templateUrl: '/views/partials/userView.html',
+        templateUrl: 'public/views/partials/userView.html',
         controller: 'UserScheduleFormSubmitController'
     }).when('/tableview', {
-        templateUrl: '/views/partials/tableview.html',
+        templateUrl: 'public/views/partials/tableview.html',
         controller: 'FinalScheduleViewController'
     }).otherwise({
-        redirectTo: 'login'
+        redirectTo: '/login'
     });
 }]);
 
