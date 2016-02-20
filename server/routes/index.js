eb008ccdeec860b53ef63a7764627be194c26730
Derek Roche <ddrochedsp@gmail.com>
2016-02-20 10:42:37 -0600

    index file routing fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,9 +5,14 @@ var path = require('path');
 
 // serves index file or whatever file is requested within the public directory
 // i.e. css files
-router.get('/*', function(req, res) {
-  var file = req.params[0] || '/views/index.html';
-  res.sendFile(path.join(process.env.PWD, '../../public', file));
+//router.get('/*', function(req, res) {
+//  var file = req.params[0] || '/views/index.html';
+//  res.sendFile(path.join(process.env.PWD, '../../public', file));
+//});
+
+router.get('/', function(req, res) {
+    var file = 'views/index.html';
+    res.sendFile(path.join(process.env.PWD, '../../public', file));
 });
 
 
