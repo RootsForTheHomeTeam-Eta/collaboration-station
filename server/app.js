d9422dadb7f22703dfad6ace43104eea7d356f7f
Derek Roche <ddrochedsp@gmail.com>
2016-02-20 00:23:37 -0600

    process.env.pwd

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ require('../db/db');
 passport.init(app);
 
 // serve static public files
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(process.env.PWD, '../public')));
 
 // include route files
 var index = require('./routes/index');
